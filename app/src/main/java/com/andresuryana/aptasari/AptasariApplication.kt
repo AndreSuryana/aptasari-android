15ccab7c1f57c923a045f3c7ff50fd7dbd374b60
AndreSuryana <Andresuryana17@gmail.com>
2023-08-03 15:30:12 +0700

    Disable dark mode compatibility

--- app/src/main/java/com/andresuryana/aptasari/AptasariApplication.kt
+++ app/src/main/java/com/andresuryana/aptasari/AptasariApplication.kt
@@ -1,7 +1,16 @@
 package com.andresuryana.aptasari
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AptasariApplication : Application()
+class AptasariApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Set UI to light mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
