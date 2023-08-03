31590799959ca15d511666266cc9beb92510a4e3
AndreSuryana <Andresuryana17@gmail.com>
2023-08-03 14:32:33 +0700

    Add new snackbar type

--- app/src/main/java/com/andresuryana/aptasari/util/SnackbarUtils.kt
+++ app/src/main/java/com/andresuryana/aptasari/util/SnackbarUtils.kt
@@ -1,12 +1,27 @@
 package com.andresuryana.aptasari.util
 
-import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.andresuryana.aptasari.R
 import com.google.android.material.snackbar.Snackbar
 
 object SnackbarUtils {
 
-    fun Fragment.showSnackbarError(@StringRes messageRes: Int) {
-        Snackbar.make(requireView(), messageRes, Snackbar.LENGTH_SHORT).show()
+    fun Fragment.showSnackbar(message: Int) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun Fragment.showSnackbarError(messagePair: Pair<Int?, String?>) {
+        val message = if (messagePair.first != null) getString(messagePair.first!!)
+        else if (messagePair.second != null) messagePair.second!!
+        else getString(R.string.error_default)
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_SHORT)
+            .setBackgroundTintList(
+                ContextCompat.getColorStateList(requireContext(), R.color.danger)
+            )
+            .setTextColor(
+                ContextCompat.getColorStateList(requireContext(), R.color.white)
+            )
+            .show()
     }
 }
